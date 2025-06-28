17b74fa00b8797e97a9249c3288258d64d7d39eb
Matias Ramirez <matias.ramirez.e@usach.cl>
2025-06-27 23:33:36 -0400

    Inicio de proyecto springboot
    se inicia la entrega crud

---
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/DbdGrupo11Application.java
@@ -0,0 +1,13 @@
+package com.DBD.Grupo_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdGrupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdGrupo11Application.class, args);
+	}
+
+}
---
+++ Grupo_11/src/test/java/com/DBD/Grupo_11/DbdGrupo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBD.Grupo_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdGrupo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
