88b79c86c844974e993ea14e42645ddc89d72673
Gerardo Perez <gerardo.perez@usach.cl>
2025-06-28 20:20:56 -0400

    Controllers configuration

--- Grupo_11/src/main/java/com/DBD/Grupo_11/Controller/CarroDeCompraController.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Controller/CarroDeCompraController.java
@@ -1,4 +1,10 @@
 package com.DBD.Grupo_11.Controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/carrodecompra")
 public class CarroDeCompraController {
 }
