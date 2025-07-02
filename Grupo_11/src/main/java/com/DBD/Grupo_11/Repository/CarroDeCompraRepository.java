4cca49cb8557bf09bdfe301f9e49b2f05280ddc6
Gerardo Perez <gerardo.perez@usach.cl>
2025-07-01 22:27:37 -0400

    Added Service Logic
    - Added Service Logic and implemented on controller

--- Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/CarroDeCompraRepository.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/CarroDeCompraRepository.java
@@ -1,4 +1,13 @@
 package com.DBD.Grupo_11.Repository;
 
-public interface CarroDeCompraRepository {
+import com.DBD.Grupo_11.Entity.CarroDeCompra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CarroDeCompraRepository extends JpaRepository <CarroDeCompra, Long> {
+    CarroDeCompra getById(Long id);
 }
--- Grupo_11/src/main/java/com/DBD/Grupo_11/Service/CarroDeCompraService.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Service/CarroDeCompraService.java
@@ -1,4 +1,31 @@
 package com.DBD.Grupo_11.Service;
 
+import com.DBD.Grupo_11.Entity.CarroDeCompra;
+import com.DBD.Grupo_11.Repository.CarroDeCompraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CarroDeCompraService {
+    @Autowired
+    private CarroDeCompraRepository carroDeCompraRepository;
+
+    public List<CarroDeCompra> getAll(){
+        return carroDeCompraRepository.findAll();
+    }
+
+    public Optional<CarroDeCompra> getById(Long id){
+        return carroDeCompraRepository.findById(id);
+    }
+
+    public CarroDeCompra save(CarroDeCompra carroDeCompra){
+        return carroDeCompraRepository.save(carroDeCompra);
+    }
+
+    public void deleteById(Long id){
+        carroDeCompraRepository.deleteById(id);
+    }
 }
