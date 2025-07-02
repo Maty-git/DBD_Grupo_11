28b2d65db4c4d25892fbfa2027b3503d6a676f58
Matias Ramirez <matias.ramirez.e@usach.cl>
2025-07-01 20:59:37 -0400

    admin controller

--- Grupo_11/src/main/java/com/DBD/Grupo_11/Controller/AdminController.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Controller/AdminController.java
@@ -1,9 +1,9 @@
 package com.DBD.Grupo_11.Controller;
 
-public class PermisoAdminController {
-@RequestMapping("/api/permisoadmin")
-@RestController
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
 }
