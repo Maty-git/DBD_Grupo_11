682c713e680e961ff44f6a4d510566fe3a5d4b04
Matias Ramirez <matias.ramirez.e@usach.cl>
2025-07-01 23:11:41 -0400

    debug

--- Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/CarroDeCompraRepository.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/CarroDeCompraRepository.java
@@ -1,21 +1,9 @@
 package com.DBD.Grupo_11.Repository;
 
-<<<<<<< HEAD
-import com.DBD.Grupo_11.Entity.Admin;
-import com.DBD.Grupo_11.Entity.CarroDeCompra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarroDeCompraRepository extends JpaRepository<CarroDeCompra, Long> {
-=======
 import com.DBD.Grupo_11.Entity.CarroDeCompra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface CarroDeCompraRepository extends JpaRepository <CarroDeCompra, Long> {
-    CarroDeCompra getById(Long id);
->>>>>>> origin/main
 }
