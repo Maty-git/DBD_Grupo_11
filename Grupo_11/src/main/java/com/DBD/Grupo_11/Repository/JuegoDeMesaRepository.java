d45994a825173d30328f9a8fcd307eef9a54d210
AxlArias <axl_arias_12@hotmail.com>
2025-07-09 00:30:55 -0400

    QUERYS
    JuegoDeMesa, ListaDeDeseos, ListaDeseosProducto, Permisos

--- Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/JuegoDeMesaRepository.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/JuegoDeMesaRepository.java
@@ -2,9 +2,17 @@ package com.DBD.Grupo_11.Repository;
 
 import com.DBD.Grupo_11.Entity.JuegoDeMesa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface JuegoDeMesaRepository extends JpaRepository<JuegoDeMesa, Long> {
     JuegoDeMesa getById(Long id);
+    @Query(value = "SELECT tipoJuegoMesa FROM JuegoDeMesa WHERE tipoJuegoMesa=tipo",  nativeQuery = true)
+    Optional<JuegoDeMesa> findByTipoJuegoMesa(@Param("tipo") String tipo);
+    @Query(value = "SELECT tipoDeJuego FROM JuegoDeMesa WHERE tipoDeJuego=tipo",   nativeQuery = true)
+    Optional<JuegoDeMesa> findByTipoDeJuego(String tipo);
 }
--- Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/ListaDeseosProductoRepository.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/ListaDeseosProductoRepository.java
@@ -2,9 +2,14 @@ package com.DBD.Grupo_11.Repository;
 
 import com.DBD.Grupo_11.Entity.ListaDeseosProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ListaDeseosProductoRepository extends JpaRepository<ListaDeseosProducto, Long> {
     ListaDeseosProducto getById(Long id);
+    @Query(value = "SELECT * FROM ListaDeseosProdicto WHERE idListaDeDeseos=idLista", nativeQuery = true)
+    Optional<ListaDeseosProducto> findByListaDeDeseos_IdListaDeDeseos(Long idLista);
 }
