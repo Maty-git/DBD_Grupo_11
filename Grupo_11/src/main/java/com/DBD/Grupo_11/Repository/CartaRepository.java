35271c7eabefc60d86fed66a3bd9b142ca405799
Gerardo Perez <gerardo.perez@usach.cl>
2025-07-09 00:17:29 -0400

    Repository

--- Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/CartaRepository.java
+++ Grupo_11/src/main/java/com/DBD/Grupo_11/Repository/CartaRepository.java
@@ -4,6 +4,12 @@ import com.DBD.Grupo_11.Entity.Carta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CartaRepository extends JpaRepository <Carta, Long> {
+public interface CartaRepository extends JpaRepository<Carta, Long> {
+    List<Carta> findByRarezaCarta(String rareza);
+    List<Carta> findByEstadoCarta(String estado);
+    List<Carta> findByAnnoCarta(int anno);
+    List<Carta> findByTipoDeJuego(String tipo);
 }
